c8482e44ed938034a91da7f9d430a1dbd033a041
fboitel <boitel.faustin@gmail.com>
2026-07-19 20:53:37 +0200

    fix: handle out of range exception when sending data

--- custom_components/energy_regulator/controller.py
+++ custom_components/energy_regulator/controller.py
@@ -15,34 +15,56 @@ class EnergyRegulatorController:
     def __init__(self, hass, entry):
         self.hass = hass
         self.entry = entry
-        self._unsubscribe = None
+        self._unsubscribe_power_setter = None
+        self._unsubscribe_mqtt_mode_setter = None
         self.store = self.hass.data["energy_regulator"][self.entry.entry_id]["store"]
         self.battery_service = BatteryService(self.store)
         self.connector_service = ConnectorService(self.hass)
 
     async def async_start(self):
-        self._unsubscribe = async_track_time_interval(
+        self._unsubscribe_power_setter = async_track_time_interval(
             self.hass,
-            self._tick,
+            self._tick_power_setter,
             timedelta(seconds=TICK_INTERVAL),
         )
 
-    async def async_stop(self):
-        if self._unsubscribe:
-            self._unsubscribe()
+        self._unsubscribe_mqtt_mode_setter = async_track_time_interval(
+            self.hass,
+            self._tick_mqtt_mode_setter,
+            timedelta(seconds=55),
+        )
 
-    async def _tick(self, now):
+    async def async_stop(self):
+        if self._unsubscribe_power_setter:
+            self._unsubscribe_power_setter()
+        if self._unsubscribe_mqtt_mode_setter:
+            self._unsubscribe_mqtt_mode_setter()
+
+    async def _tick_power_setter(self, now):
         state = self.hass.states.get(SHELLY_DEVICE_ID)
         if state:
             self.store.shelly_power = float(state.state)
             
         self.battery_service.update_power()
         await self.send_battery_powers()
+    
+    async def _tick_mqtt_mode_setter(self, now):
+        await self.set_mqtt_mode()
+
+    async def set_mqtt_mode(self):
+        for battery in BATTERIES.keys():
+            if battery in self.store.active_batteries:
+                entity_id = BATTERIES[battery]
+                await self.connector_service.set_mqtt_mode(entity_id)
 
     async def send_battery_powers(self):
         for battery in BATTERIES.keys():
             if battery in self.store.active_batteries:
                 power = self.store.battery_powers[battery]
                 entity_id = BATTERIES[battery]
-                await self.connector_service.send_number(entity_id, power)
-                await self.connector_service.set_mqtt_mode(entity_id)
+                try:
+                    print(f"Sending power {power} to {entity_id}")
+                    await self.connector_service.send_number(entity_id, power)
+                except Exception as e:
+                    _LOGGER.error(f"Error sending power to {entity_id}: {e}")
+                    print(f"Error sending power to {entity_id}: {e}")
